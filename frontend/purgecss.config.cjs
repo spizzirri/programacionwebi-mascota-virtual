b909ae3d470dda4c396b84810a0fa96aa5e5b77c
damian spizzirri <damianspizzirri@gmail.com>
2026-03-01 17:19:44 -0300

    configura purgecss

---
+++ frontend/purgecss.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+    content: ['index.html', 'src/**/*.{ts,js,html}'],
+    css: ['src/styles/**/*.css'],
+    rejected: true,
+    rejectedCss: true,
+    output: './src/styles/'
+}
